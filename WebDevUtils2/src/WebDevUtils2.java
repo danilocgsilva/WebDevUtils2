bf44a12982f19c73f62fdd2e2fecee09025422ea
Danilo Carlos de Góes Silva <danilocgsilva@gmail.com>
2016-03-26 21:34:26 -0300

    Now the software removes the comments from the hosts files.

--- WebDevUtils2/src/WebDevUtils2.java
+++ WebDevUtils2/src/WebDevUtils2.java
@@ -1,22 +1,21 @@
 import FTP.FTPDownload;
+import content.ReadContentHandledException;
+import hosts.Hosts;
 
 public class WebDevUtils2 {
 
 	public static void main(String[] args) {
 	
-		/*
 		String content = null;
 		ReadContentHandledException rContent = new ReadContentHandledException();
 		content = rContent.readFile("D://Users//Danilo//Temporários//cteste.txt");
-		System.out.println(content);
-		*/
+		System.out.println(content);	
 		
-		/*
 		Hosts hosts = new Hosts();
 		System.out.println(hosts.getContent());
-		*/
 		
-		FTPDownload ftpd = new FTPDownload();
-		ftpd.download();
+		
+		/*FTPDownload ftpd = new FTPDownload();
+		ftpd.download();*/
 	}
 }
--- WebDevUtils2/src/hosts/Hosts.java
+++ WebDevUtils2/src/hosts/Hosts.java
@@ -12,11 +12,26 @@ public class Hosts {
 		this.systemOS = System.getProperty("os.name");
 		if (this.systemOS.toLowerCase().contains("windows")) {
 			this.hostPath = "C://Windows//System32//drivers//etc//hosts";
+		} else if (this.systemOS.toLowerCase().contains("linux")) {
+			this.hostPath = "//etc//hosts";
 		} else {
 			this.hostPath = "";
 		}
 		ReadContentHandledException rContent = new ReadContentHandledException();
 		this.content = rContent.readFile(this.hostPath);
+		
+		String eachLine[] = this.content.split("\\r?\\n");
+		this.content = "";
+		
+		for (int i = 0; i < eachLine.length; i++) {
+			String eachLineLoop = eachLine[i];
+			int commentSeparatorPosition = eachLineLoop.indexOf('#');
+			if (commentSeparatorPosition != -1) {
+				eachLineLoop = eachLineLoop.substring(0, commentSeparatorPosition);
+			}
+			
+			this.content += eachLineLoop + "\n";
+		}
 	}
 	
 	public String getContent() {
