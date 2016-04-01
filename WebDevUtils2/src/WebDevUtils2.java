2112b0e2351d084c6095074500d45c2edb291a84
Danilo <Danilo@W7HPPavilion>
2016-03-31 22:25:29 -0300

    Now the program is download sucefully a ftp file using the Apache Commons. Try 2.

--- WebDevUtils2/src/WebDevUtils2.java
+++ WebDevUtils2/src/WebDevUtils2.java
@@ -1,7 +1,15 @@
-import FTP.FTPDownload;
-import FTP.FTPDownloadApacheNet;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTP;
+
+import java.io.IOException;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 import content.ReadContentHandledException;
 import hosts.Hosts;
+
 
 public class WebDevUtils2 {
 
@@ -25,9 +33,41 @@ public class WebDevUtils2 {
 		
 		ftpd.download("/cgi-bin/ftp/hwftp.txt", "D:\\Users\\Danilo\\Temporários\\hwftp5.txt");*/
 		
-		/* Using the Apache Net FTP Cliente */
 		
-		FTPDownloadApacheNet ftpc = new FTPDownloadApacheNet(); 
+		
+		/* Using the Apache Net FTP Cliente */
+        FTPClient ftpClient = new FTPClient();
+        try {
+ 
+            ftpClient.connect("www.danilocgsilva.me", 21);
+            ftpClient.login("www.danilocgsilva.me", "yat178");
+            ftpClient.enterLocalPassiveMode();
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+ 
+            // APPROACH #1: using retrieveFile(String, OutputStream)
+            String remoteFile1 = "/cgi-bin/ftp/hwftp.txt";
+            File downloadFile1 = new File("C:\\Users\\Danilo\\temporarios\\hwftp1.txt");
+            OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
+            boolean success = ftpClient.retrieveFile(remoteFile1, outputStream1);
+            outputStream1.close();
+ 
+            if (success) {
+                System.out.println("File #1 has been downloaded successfully.");
+            }
+ 
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (ftpClient.isConnected()) {
+                    ftpClient.logout();
+                    ftpClient.disconnect();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
 		
 	}
 }
